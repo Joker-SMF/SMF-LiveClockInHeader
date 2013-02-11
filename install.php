21fa1b6b441aa5403947b3a4a36edfd719190b8c
Joker SMF <makerzzz.zzzz@gmail.com>
2013-02-11 17:10:07 +0530

    inserting timezones

--- install.php
+++ install.php
@@ -80,6 +80,47 @@ foreach ($general_settings as $key => $value) {
     );
 }
 
+$live_clock_timezones = array(
+    'Eniwetok, Kwajalein' => '-12:00',
+    'Midway Island, Samoa' => '-11:00',
+    'Hawaii' => '-10:00',
+    'Alaska' => '-9:00',
+    'Pacific Time (US &amp; Canada)' => '-8:00',
+    'Mountain Time (US &amp; Canada)' => '-7:00',
+    'Central Time (US &amp; Canada), Mexico City' => '-6:00',
+    'Eastern Time (US &amp; Canada), Bogota, Lima' => '-5:00',
+    'Atlantic Time (Canada), Caracas, La Paz' => '-4:00',
+    'Newfoundland' => '-3:30',
+    'Brazil, Buenos Aires, Georgetown' => '-3:00',
+    'Mid-Atlantic' => '-2:00',
+    'Azores, Cape Verde Islands' => '-1:00',
+    'Western Europe Time, London, Lisbon, Casablanca' => '0:00',
+    'Brussels, Copenhagen, Madrid, Paris' => '+1:00',
+    'Kaliningrad, South Africa' => '+2:00',
+    'Baghdad, Riyadh, Moscow, St. Petersburg' => '+3:00',
+    'Tehran' => '+3:30',
+    'Abu Dhabi, Muscat, Baku, Tbilisi' => '+4:00',
+    'Kabul' => '+4:30',
+    'Ekaterinburg, Islamabad, Karachi, Tashkent' => '+5:00',
+    'Bombay, Calcutta, Madras, New Delhi' => '+5:30',
+    'Kathmandu' => '+5:45',
+    'Almaty, Dhaka, Colombo' => '+6:00',
+    'Bangkok, Hanoi, Jakarta' => '+7:00',
+    'Beijing, Perth, Singapore, Hong Kong' => '+8:00',
+    'Tokyo, Seoul, Osaka, Sapporo, Yakutsk' => '+9:00',
+    'Adelaide, Darwin' => '+9:30',
+    'Eastern Australia, Guam, Vladivostok' => '+10:00',
+    'Magadan, Solomon Islands, New Caledonia' => '+11:00',
+    'Auckland, Wellington, Fiji, Kamchatka' => '+12:00',
+);
+
+foreach ($live_clock_timezones as $key => $value) {
+    $smcFunc['db_insert']('ignore',
+        '{db_prefix}live_clock_timezones', array('zone_name' => 'string', 'zone_diff' => 'string'),
+        array($key, $value), ''
+    );
+}
+
 add_integration_function('integrate_pre_include', '$sourcedir/LiveClockHooks.php');
 add_integration_function('integrate_admin_areas', 'LC_addAdminPanel');
 add_integration_function('integrate_actions', 'LC_addAction', true);
